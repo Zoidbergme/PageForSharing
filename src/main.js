3bf1afe6bb8c10f73bcdc1bfb0bd0f0e823325dd
Zoidbergme <suxun1202@gmail.com>
2018-05-02 10:34:58 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import {Button} from 'element-ui'
+import App from './App'
+import axios from 'axios'
+import './assets/reset.css'
+
+
+Vue.prototype.$http = axios
+Vue.config.productionTip = false
+Vue.use(Button)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
